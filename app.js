91b4deb54a8f84220c5e3abc415d15d922ea11fe
Lyz03 <lizoe@netcourrier.com>
2021-12-08 09:37:10 +0100

    exo 93 v2

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const alertButton = document.getElementById('alert');
 const somme = document.getElementById('somme');
 
+
+window.addEventListener("keypress", event => {
+    if (event.key === 'Enter') {
+        alert('Hello world');
+        alertButton.className = 'pink'
+    }
+})
+
 alertButton.addEventListener("click", (event) => {
     alert('Hello world');
-    event.target.style.color = 'red'
+    event.target.className = 'pink'
 });
 
 somme.addEventListener("click", event => {
     let newDiv = document.createElement('div');
     newDiv.innerText = (5 + 6).toString();
     document.querySelector('div').appendChild(newDiv)
-    event.target.style.color = "red";
+    event.target.className = 'pink';
 });
 
 alertButton.addEventListener('mouseenter', event => {
-    event.target.style.color = 'green'
+    event.target.className = 'violet';
 });
 
 somme.addEventListener('mouseenter', event => {
-    event.target.style.color = 'green'
+    event.target.className = 'violet';
 });
 
 alertButton.addEventListener('mouseleave', event => {
-    event.target.style.color = 'blue'
+    event.target.className = 'blue';
 });
 
 somme.addEventListener('mouseleave', event => {
-    event.target.style.color = 'blue'
+    event.target.className = 'blue';
 });
